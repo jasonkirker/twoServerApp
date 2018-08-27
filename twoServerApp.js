abff0fd6381910ea14d6e780f6bb76a744e96e99
Vance <jasonkirker@users.noreply.github.com>
2018-08-27 11:12:33 -0500

    finished the app

--- twoServerApp.js
+++ twoServerApp.js
@@ -1,13 +1,26 @@
 var http = require("http");
 
-var PORT = 7000;
+var nice_PORT = 7000;
+var mean_PORT = 7500;
 
-function handleRequest(request, response){
-    response.end("You are doing great in life!" + request.url)
+// good_PORT 7000 - positive notes
+function handleRequest_nice(request, response){
+    response.end("You're doing great in life.");
+}    
+
+function handleRequest_mean(request, response) {
+    response.end("You need to figure your life out.");
 }
+// ==========
 
-var server = http.createServer(handleRequest);
+var server_nice = http.createServer(handleRequest_nice);
+var server_mean = http.createServer(handleRequest_mean);
+// ==========
 
-server.listen(PORT, function(){
-    console.log(`server is listening on http://localhost:${PORT}`);
-})
+server_nice.listen(nice_PORT, function(){
+    console.log(`Server listening on http://localhost:${nice_PORT}`);
+});
+
+server_mean.listen(mean_PORT, function(){
+    console.log(`Server listening on http://localhost:${mean_PORT}`);
+});
